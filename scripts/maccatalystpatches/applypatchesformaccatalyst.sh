dbce95dc6c08374361fab0c9b8659a02e33571ab
Marcos Rodriguez Vélez <marcosrodriguez@me.com>
2021-07-18 13:10:44 -0400

    WIP

--- scripts/maccatalystpatches/applypatchesformaccatalyst.sh
+++ scripts/maccatalystpatches/applypatchesformaccatalyst.sh
@@ -5,5 +5,7 @@ echo "Re-installing node_modules"
 npm i
 echo "Applying patch for react-native-xcode.sh"
 sed -i '' 's/--platform "$BUNDLE_PLATFORM"/--platform "ios"/g' ./node_modules/react-native/scripts/react-native-xcode.sh
+echo "Deleting torrific.js content"
+echo > blue_modules/torrific.js
 echo ""
 echo "NOTE: react-native-tor is not currently compatible with Mac Catalyst. If you are running macOS Catalina, you will need to remove the iOS 14 Widgets from the project targets."
